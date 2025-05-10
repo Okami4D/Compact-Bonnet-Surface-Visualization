eef9e18565ba3b660482deb1ed3f4e09325ae4d6
Kiba Mangandango <okami4d@gmail.com>
2025-05-10 14:49:37 +0200

    Added "Quality of Life" buttons into the interactive window

--- playground.jl
+++ playground.jl
@@ -1,27 +1,65 @@
+using GLMakie
 include("SurfaceViz.jl")
 
 # Plot Setup
 fig = Figure(size=(1920, 1080), scenekw = (lights = [DirectionalLight(RGBf(1, 1, 1), Vec3f(-1, 0, 0))],))
+ang = Observable(1.01 * pi)
+
 ax = Axis3(
     fig[1, 1], 
     aspect = :data, 
     perspectiveness = 0.8, 
     clip=false
 )
+
 hidedecorations!(ax)
 hidespines!(ax)
 
+activeMesh = []
+
+# Setup the Buttons
+fig[2, 1] = buttongrid = GridLayout(tellwidth = false)
+buttons = buttongrid[1, 1:4] = [
+    Button(fig, label = "Surface", width = 150, height = 70, fontsize = 30),
+    Button(fig, label = "Wireframe", width = 150, height = 70, fontsize = 30),
+    Button(fig, label = "Save PNG", width = 150, height = 70, fontsize = 30),
+    Button(fig, label = "Save OBJ", width = 150, height = 70, fontsize = 30)
+]
+
+on(buttons[1].clicks) do b
+    empty!(ax)
+    mesh!(ax, activeMesh, specular = 0.4, diffuse = 0.7)
+    fig
+end
+on(buttons[2].clicks) do b
+    empty!(ax)
+    wireframe!(ax, activeMesh, linewidth = 0.5)
+    fig
+end
+on(buttons[3].clicks) do b
+    save("helicoid.png", fig, size = (1920, 1080))
+end
+on(buttons[4].clicks) do b
+
+    save("helicoid.obj", hel)
+end
 
 
 
 
-
-#mesh!(ax, createPlane(2, 2))
-#mesh!(ax, createSphere(2))
-
-hel = createHelicoid(2, 10, 60, 400)
-mesh!(ax, hel, specular = 0.4, diffuse = 0.7)
-
-#save("helicoid.obj", hel)
-
+# Generate Mesh 
+activeMesh = createHelicoid(5, 1, 6.28, 35)
+mesh!(ax, activeMesh, specular = 0.4, diffuse = 0.7)
 fig
+
+#=
+#Rotating Camera around center
+fps = 60
+nframes = 1200
+
+for i in 1:nframes
+    ang[] = ang[] + 0.01
+    sleep(1 / fps)
+end
+#save("helicoid.obj", hel)
+=#
