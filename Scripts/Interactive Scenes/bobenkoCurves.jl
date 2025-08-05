ef3c5c5228552b7e22bcdde1f0813dfab9eea556
Kiba Mangandango <okami4d@gmail.com>
2025-08-05 17:46:56 +0200

    Formalized bobenkoCurve visualization

--- Scripts/Interactive Scenes/bobenkoCurves.jl
+++ Scripts/Interactive Scenes/bobenkoCurves.jl
@@ -4,58 +4,69 @@ include("../../Tools/ParametricSurfaceTools.jl")
 include("../../Tools/ParametricCurveTools.jl")
 include("../../Tools/isothermicCylinderTools.jl")
 
+# Constants
+tau = 0.5 + 0.3 * im
+
+
+
+
 # Plot Setup
 fig = Figure(size=(1200, 800))
 
 ax = Axis(
-    fig[1, 2],
-    limits =(-3, 3, -4, 2),
+    fig[1, 1],
+    limits =(-2, 4, -4, 2),
     title = "Isothermic Cylinder Curvature Lines - Changing Family Parameter"
 )
 hidedecorations!(ax)
 hidespines!(ax)
 
-
-tau = 0.5 + 0.3 * im
-
-
+sliderGrid = GridLayout(fig[2, 1], columns = 2, rows = 4, showaxis=false)
 
 familySlide = Slider(
-    fig[2, 2], 
+    sliderGrid[1, 2], 
     range = 0:0.01:2, 
     startvalue = 0.5, 
     update_while_dragging=true, 
 )
-
 familySlideText = lift(familySlide.value) do num
     string("w = ", round.(num, digits = 2))
 end
-Label(fig[2, 1], familySlideText, tellwidth = false)
-
+Label(sliderGrid[1, 1], familySlideText, tellwidth = false)
 
 intervalSlide = IntervalSlider(
-    fig[3, 2], 
+    sliderGrid[2, 2], 
     range = LinRange(-5 *pi, 5 * pi, 1000), 
     startvalues = (0, 2 * pi), 
 )
-
 intervalSlideText = lift(intervalSlide.interval) do int
     string("Interval: ", round.(int, digits = 2))
 end
-Label(fig[3, 1], intervalSlideText, tellwidth = false)
+Label(sliderGrid[2, 1], intervalSlideText, tellwidth = false)
+
 
 omegaSlide = Slider(
-    fig[4, 2], 
+    sliderGrid[3, 2], 
     range = 0:0.001:2, 
     startvalue = findOmegaRhombic(tau), 
     update_while_dragging=true, 
 )
-
 omegaSlideText = lift(omegaSlide.value) do int
     string("Omega = ", round.(int, digits = 2))
 end
-Label(fig[4, 1], omegaSlideText, tellwidth = true)
+Label(sliderGrid[3, 1], omegaSlideText, tellwidth = true)
 
+togl = Observable{Bool}(true)
+rhombicToggle = ToggleButton(
+    sliderGrid[4, 2], 
+    label = "Rhombic Lattice", 
+    startvalue = true, 
+    width = 150,
+)
+
+on(rhombicToggle.value) do val
+    togl[] = val
+end
 
 curve_obs = lift(familySlide.value, intervalSlide.interval, omegaSlide.value) do w_val, int, omegaVal
     t_vals = LinRange(int..., 1000)
