9ccaf1338ba7a46b93f919aed7270198b2e74ea1
Kiba Mangandango <okami4d@gmail.com>
2025-08-28 16:56:54 +0200

    Added Documenter.jl for Documentation into a Github Pages file. For this I needed to change the Folder Structure

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter
+
+makedocs(
+    sitename = "Parametric Surface Visualization - Compact Bonnet Pairs",
+    authors = "Kiba Mangandango",
+    pages = [
+    "Home"     => "index.md",
+    "Theory"   => "theory.md",
+    "Examples" => "examples.md",
+    "API"      => "api.md",
+    ],
+    # optional: configure HTML/math; leave default for now
+    format = Documenter.HTML()
+)
+
+# deploy to gh-pages (replace user/repo)
+deploydocs(repo = "github.com/Okami4D/Compact-BonnetSurface-Visualization.git")
