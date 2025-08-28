f3775ad19fb100179fd9ec79df877b556bce2997
Documenter.jl <documenter@juliadocs.github.io>
2025-08-28 15:25:11 +0000

    build based on ca557e1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"This Project was built under the supervision of Prof. Dr. Tim Hoffmann at the Technical University of Munich. It aims to visualize the paper \"Compact Bonnet Pairs: Isometric Tori with the same Curvatures\" and the applied techniques in interactive GLMakie scenes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is some Test MathJax $ F(x, y) = 2 x + 2 x^2 + y^4 $","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"This Project was built under the supervision of Prof. Dr. Tim Hoffmann at the Technical University of Munich. It aims to visualize the paper \"Compact Bonnet Pairs: Isometric Tori with the same Curvatures\" and the applied techniques in interactive GLMakie scenes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is some Test MathJax $$ F(x, y) = 2 x + 2 x^2 + y^4 $$","category":"page"}]
 }
