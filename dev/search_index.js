90fbc080d2dd5ef4cfdcb1554245034a4f3c1151
Documenter.jl <documenter@juliadocs.github.io>
2025-08-28 19:48:21 +0000

    build based on 85a40a2

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"This Project was built under the supervision of Prof. Dr. Tim Hoffmann at the Technical University of Munich. It aims to visualize the paper \"Compact Bonnet Pairs: Isometric Tori with the same Curvatures\" and the applied techniques in interactive GLMakie scenes.","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"This Project was built under the supervision of Prof. Dr. Tim Hoffmann at the Technical University of Munich. It aims to visualize the paper \"Compact Bonnet Pairs: Isometric Tori with the same Curvatures\" and the applied techniques in interactive GLMakie scenes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"Math Test det(lambda I_k - mathcalJ_k) = begincases(lambda^2-1)^frack2 quad k text even (lambda - 1)(lambda^2-1)^frack-12  quad k text oddendcases","category":"page"}]
 }
